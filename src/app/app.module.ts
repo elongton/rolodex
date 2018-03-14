917e536556572dac09847995ab34122fef861f70
Max Longton <elongton@vt.edu>
2018-03-14 02:11:02 -0400

    added Contact model and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactItemComponent,
+    ContactListComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,12 @@
+export class Contact {
+  constructor(public first_name: string,
+              public last_name: string,
+              public email: string,
+              public phone: string,
+              public website: string,
+              public street_address: string,
+              public city: string,
+              public state: string,
+              public zip: string,
+              public notes: string){}
+}
