15f7ba4140b913e548793828d4e16a4f51224127
Max Longton <elongton@vt.edu>
2018-03-19 23:39:18 -0400

    creating header, sidebar, and contacts template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/contacts', pathMatch: 'full'},
+  {path: 'contacts', component: ContactListComponent},
+]
 
 
 @NgModule({
@@ -13,10 +21,13 @@ import { ContactDetailComponent } from './contacts/contact-detail/contact-detail
     AppComponent,
     ContactItemComponent,
     ContactListComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
