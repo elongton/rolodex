421693961bf48d4425a399553135c66196634bf6
Max Longton <elongton@vt.edu>
2018-03-27 09:00:06 -0400

    updated the sidebar to material

--- src/app/contacts/contact-form-new/contact-form-new.component.ts
+++ src/app/contacts/contact-form-new/contact-form-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form-new',
@@ -10,6 +11,10 @@ export class ContactFormNewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 
 }
