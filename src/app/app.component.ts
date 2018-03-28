3da445f8ff05d061e1c8dc26cb5aad9449d79180
Enrique Longton <enrique.longton@gmail.com>
2018-03-28 16:06:46 -0400

    added a toggleable sidenav for forms and the like

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Rolodex';
+  mode = new FormControl('over');
+  shouldRun = true;
 }
