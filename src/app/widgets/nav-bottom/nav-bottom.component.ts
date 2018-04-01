274a756f622fb702afc881ffe43438ba3c5b0865
Max Longton <elongton@vt.edu>
2018-04-01 10:37:37 -0400

    added *=>void state to add buttons transition

--- src/app/widgets/nav-bottom/nav-bottom.component.ts
+++ src/app/widgets/nav-bottom/nav-bottom.component.ts
@@ -11,8 +11,10 @@ import { GlobalService } from '../../shared/global.service';
     trigger('buttonState', [
       state('inactive', style({opacity: 0})),
       state('active',   style({opacity: 1})),
-      transition('inactive => active',  animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out')),
+
+      transition('inactive => active',  animate('100ms')),
+      transition('active => inactive', animate('100ms')),
+      transition('* => void', animate('100ms', style({opacity: 0}))),
     ])
   ]
 })
